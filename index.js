7b4295e43fb724e37fd814ddee8946cd7b1f50ac
Barny-03 <barnihicham17@gmail.com>
2024-05-10 07:11:37 -0400

    upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! Hicham BARNY</h1>')
+  res.send('<h1>Hicham BARNY</h1>')
 })
 
 const PORT = 8080
