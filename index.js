b37ef660955480f36cb154d9056e08e0a31bc8f9
Barny-03 <128234613+Barny-03@users.noreply.github.com>
2024-05-11 11:15:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>check ci/cd pipelin workflow</h1>')
+  res.send('<h1>it's working after commit from github itself</h1>')
 })
 
 const PORT = 8080
