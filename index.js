dbb0766668a0c6eff6cbe1597bd2fa4fa911ab93
Barny-03 <barnihicham17@gmail.com>
2024-05-11 05:55:42 -0400

    watchtower check v2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Watchtower hh</h1>')
+  res.send('<h1>check ci/cd pipelin workflow</h1>')
 })
 
 const PORT = 8080
